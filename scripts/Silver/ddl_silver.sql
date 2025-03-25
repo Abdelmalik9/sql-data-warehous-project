215f285fcf72aae96f80bce44f5327aa2ec91bd5
ABD_EL_MALIK <121776741+Abdelmalik9@users.noreply.github.com>
2025-03-25 06:51:51 +0100

    Update ddl_silver.sql

--- scripts/Silver/ddl_silver.sql
+++ scripts/Silver/ddl_silver.sql
@@ -15,14 +15,14 @@ if OBJECT_ID ('Datawarehouse.silver.crm_cust_info', 'U') is not null
 GO
 
 CREATE TABLE Datawarehouse.silver.crm_cust_info (
-	cst_id              int,
-	cst_key             nvarchar (50),
-	cst_firstname       nvarchar (50),
-	cst_lastname        nvarchar (50),
-	cst_marital_status  nvarchar (50),
-	cst_gndr            nvarchar (50),
-	cst_create_date     date,
-	dwh_create_date     datetime2 default getdate()
+	cst_id              		int,
+	cst_key             		nvarchar (50),
+	cst_firstname      		nvarchar (50),
+	cst_lastname        		nvarchar (50),
+	cst_marital_status  		nvarchar (50),
+	cst_gndr            		nvarchar (50),
+	cst_create_date     		date,
+	dwh_create_date     		datetime2 default getdate()
 );
 
 GO
@@ -39,9 +39,9 @@ create table Datawarehouse.silver.crm_prd_info(
 	prd_nm				nvarchar (50),
 	prd_cost			int,
 	prd_line			nvarchar (50),
-	prd_start_dt		date,
+	prd_start_dt			date,
 	prd_end_dt			date,
-	dwh_create_date     datetime2 default getdate()
+	dwh_create_date    		datetime2 default getdate()
 );
 
 GO
@@ -55,13 +55,13 @@ create table Datawarehouse.silver.crm_sales_details(
 	sls_ord_num			nvarchar (50),
 	sls_prd_key			nvarchar (50),
 	sls_cust_id			int,
-	sls_order_dt		date,
+	sls_order_dt			date,
 	sls_ship_dt			date,
 	sls_due_dt			date,
 	sls_sales			int,
-	sls_quantity		int,
+	sls_quantity			int,
 	sls_price			int,
-	dwh_create_date     datetime2 default getdate()
+	dwh_create_date    		datetime2 default getdate()
 );
 
 GO
@@ -73,10 +73,10 @@ GO
 
 create table datawarehouse.silver.erp_cust_az12(
 
-	cid					nvarchar (50),
+	cid				nvarchar (50),
 	bdate				date,
-	gen					nvarchar (50),
-	dwh_create_date     datetime2 default getdate()
+	gen				nvarchar (50),
+	dwh_create_date    		datetime2 default getdate()
 
 );
 
@@ -89,9 +89,9 @@ GO
 
 create table Datawarehouse.silver.erp_loc_a101(
 
-	cid					nvarchar (50),
+	cid				nvarchar (50),
 	cntry				nvarchar (50),
-	dwh_create_date     datetime2 default getdate()
+	dwh_create_date     		datetime2 default getdate()
 
 );
 
@@ -104,11 +104,11 @@ GO
 
 create table Datawarehouse.silver.erp_px_cat_g1v2(
 
-	id_					nvarchar(50),
-	cat					nvarchar(50),
+	id_				nvarchar(50),
+	cat				nvarchar(50),
 	subcat				nvarchar(50),
 	maintenance			nvarchar(50),
-	dwh_create_date     datetime2 default getdate()
+	dwh_create_date     		datetime2 default getdate()
 
 );
 
